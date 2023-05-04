490801436241700667e98a316d8c5a30b5ded3af
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-05-04 20:14:32 +0200

    weather

---
+++ src/features/weather.js
@@ -0,0 +1,39 @@
+const apiKey = "?key=62d7f1dc6fcb4c748e2115014231104";
+const apiUrl = "https://api.weatherapi.com/v1/forecast.json";
+
+export async function checkWeatherOne(city){
+    try {
+    const response = await fetch(apiUrl+`${apiKey}`+'&q='+ city +'&aqi=yes&days=2');
+    const data = await response.json();
+    console.log(data);
+    const siteIco = document.querySelector('.weatherOne__ico').src = data.current.condition.icon;
+    const siteCity = document.querySelector('.weatherOne__city').innerHTML = city;
+    const siteTemp = document.querySelector('.weatherOne__temperature').innerHTML = Math.round(data.current.temp_c);
+    } catch (error) {
+      console.error(error);
+    }}
+
+export async function checkWeatherTwo(city){
+    try {
+    const response = await fetch(apiUrl+`${apiKey}`+'&q='+ city +'&aqi=yes&days=2');
+    const data = await response.json();
+    console.log(data);
+    const siteIco = document.querySelector('.weatherTwo__ico').src = data.current.condition.icon;
+    const siteCity = document.querySelector('.weatherTwo__city').innerHTML = city;
+    const siteTemp = document.querySelector('.weatherTwo__temperature').innerHTML = Math.round(data.current.temp_c);
+}   catch (error) {
+    console.error(error);
+    }}
+
+export async function checkWeatherThree(city){
+    try {
+    const response = await fetch(apiUrl+`${apiKey}`+'&q='+ city +'&aqi=yes&days=2');
+    const data = await response.json();
+    console.log(data);
+    const siteIco = document.querySelector('.weatherThree__ico').src = data.current.condition.icon;
+    const siteCity = document.querySelector('.weatherThree__city').innerHTML = city;
+    const siteTemp = document.querySelector('.weatherThree__temperature').innerHTML = Math.round(data.current.temp_c);
+}   catch (error) {
+    console.error(error);
+}}
+            
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import { countdown } from "./features/timer";
-
+import { checkWeatherOne, checkWeatherTwo, checkWeatherThree } from "./features/weather"
+const cityOne = 'Agia Pelagia'
+const cityTwo = 'Heraklion'
+const cityThree = 'Chania'
 countdown()
+checkWeatherOne(cityOne)
+checkWeatherTwo(cityTwo)
+checkWeatherThree(cityThree)

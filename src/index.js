0da08993853f093697c117527ed20421c5432a87
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-05-07 00:31:07 +0200

    fx

--- src/index.js
+++ src/index.js
@@ -91,10 +91,10 @@ const listFour = document.querySelector('.modal__list-Four');
 const listFive = document.querySelector('.modal__list-Five');
 const listSix = document.querySelector('.modal__list-Six');
 
-listOne.addEventListener('click', checkElement = (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
-listTwo.addEventListener('click', checkElement = (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
-listThree.addEventListener('click', checkElement = (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
-listFour.addEventListener('click', checkElement = (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
-listFive.addEventListener('click', checkElement = (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
-listSix.addEventListener('click', checkElement = (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
+listOne.addEventListener('click', (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
+listTwo.addEventListener('click', (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
+listThree.addEventListener('click', (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
+listFour.addEventListener('click', (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
+listFive.addEventListener('click', (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
+listSix.addEventListener('click', (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
 

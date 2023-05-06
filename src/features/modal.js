a97a53deffcd92841107efbac3c32dc5a88ef511
Marcinn88 <111724779+Marcinn88@users.noreply.github.com>
2023-05-07 00:15:50 +0200

    fx

--- src/features/modal.js
+++ src/features/modal.js
@@ -1,6 +1,6 @@
-export const mainModalWrapper = document.querySelector('.main__modal-wrapper')
-export const closeBtn = document.querySelector('#closeModal');
-export const showBtn = document.querySelector('#showModal')
+// export const mainModalWrapper = document.querySelector('.main__modal-wrapper')
+// export const closeBtn = document.querySelector('#closeModal');
+// export const showBtn = document.querySelector('#showModal')
 
 // closeBtn.addEventListener('click', closeFunction =()=> {
 //     mainModalWrapper.classList.add('is-hidden-modal');
@@ -14,19 +14,19 @@ export const showBtn = document.querySelector('#showModal')
 
 
 // Add a "checked" symbol when clicking on a list item
-const listOne = document.querySelector('.modal__list-One');
-const listTwo = document.querySelector('.modal__list-Two');
-const listThree = document.querySelector('.modal__list-Three');
-const listFour = document.querySelector('.modal__list-Four');
-const listFive = document.querySelector('.modal__list-Five');
-const listSix = document.querySelector('.modal__list-Six');
+// const listOne = document.querySelector('.modal__list-One');
+// const listTwo = document.querySelector('.modal__list-Two');
+// const listThree = document.querySelector('.modal__list-Three');
+// const listFour = document.querySelector('.modal__list-Four');
+// const listFive = document.querySelector('.modal__list-Five');
+// const listSix = document.querySelector('.modal__list-Six');
 
-listOne.addEventListener('click', checkElement = (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
-listTwo.addEventListener('click', checkElement = (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
-listThree.addEventListener('click', checkElement = (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
-listFour.addEventListener('click', checkElement = (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
-listFive.addEventListener('click', checkElement = (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
-listSix.addEventListener('click', checkElement = (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
+// listOne.addEventListener('click', checkElement = (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
+// listTwo.addEventListener('click', checkElement = (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
+// listThree.addEventListener('click', checkElement = (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
+// listFour.addEventListener('click', checkElement = (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
+// listFive.addEventListener('click', checkElement = (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
+// listSix.addEventListener('click', checkElement = (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
 
 
 
--- src/index.js
+++ src/index.js
@@ -82,3 +82,19 @@ const visited ={
     3:{visited:0},
     4:{visited:2}
 }
+
+
+const listOne = document.querySelector('.modal__list-One');
+const listTwo = document.querySelector('.modal__list-Two');
+const listThree = document.querySelector('.modal__list-Three');
+const listFour = document.querySelector('.modal__list-Four');
+const listFive = document.querySelector('.modal__list-Five');
+const listSix = document.querySelector('.modal__list-Six');
+
+listOne.addEventListener('click', checkElement = (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
+listTwo.addEventListener('click', checkElement = (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
+listThree.addEventListener('click', checkElement = (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
+listFour.addEventListener('click', checkElement = (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
+listFive.addEventListener('click', checkElement = (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
+listSix.addEventListener('click', checkElement = (ev) => {if (ev.target.tagName === 'LI') {ev.target.classList.toggle('checked')}}, false);
+
